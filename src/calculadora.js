e6f39ffec07f5fd4b88722add4f31c7fa598adb0
Raphael <phael92br@gmail.com>
2023-05-30 13:20:02 -0300

    first commit

---
+++ src/calculadora.js
@@ -0,0 +1,13 @@
+// Importação / Referência a Bibliotecas
+
+// Classe (Opcional)
+
+// Funções ou Métodos
+
+// var: variavel
+// let: variavel aperfeioçoada
+// const: constante 
+
+let somarDoisNumeros = (num1, num2) => {
+    
+}
